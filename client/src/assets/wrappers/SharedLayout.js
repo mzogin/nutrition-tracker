a6926495bedc5a4c6bb855e8b97b7e1d77a44382
Michael Zogin <mdzogin@gmail.com>
2023-05-30 18:30:26 -0400

    updated media query pt3

--- client/src/assets/wrappers/SharedLayout.js
+++ client/src/assets/wrappers/SharedLayout.js
@@ -8,15 +8,14 @@ const Wrapper = styled.section`
   .dashboard-page {
     width: 90vw;
     margin: 0 auto;
-    /* padding: 2rem 0; */
     padding: 1rem 0;
   }
   @media (min-width: 992px) {
-    .dashboard-page {
-      width: 90%;
-    }
     .dashboard {
       grid-template-columns: auto 1fr;
+    }
+    .dashboard > .dashboard-page {
+      width: 90%;
     }
   }
 `
